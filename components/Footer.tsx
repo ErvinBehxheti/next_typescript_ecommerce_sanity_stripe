d568c78767bb575bc531355e8acce863df281fb9
Ervin Behxheti <94991585+ErvinBehxheti@users.noreply.github.com>
2024-02-16 18:43:06 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
 const Footer: NextPage = () => {
   return (
     <div className="footer-container">
-      <p>2022 Novagency Shopping</p>
+      <p>2022 Ervin Behxheti Shopping</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
